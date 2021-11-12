3ca4db3efe65a45b48e6fc8d6a15f271bca5d7c0
Sebastian Gorecki <sebastian.gorecki@olx.pl>
2021-11-12 22:38:39 +0100

    Adds Go/clock solution

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,19 +1,48 @@
 package clock
 
-// Define the Clock type here.
+import "fmt"
 
+// Clock represents time.
+type Clock struct {
+	minutes int
+}
+
+const dayMinutes = 1440
+const hourMinutes = 60
+
+// New creates clock from hours and minutes
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	return Clock{minutes: toMinutes(h, m)}
 }
 
+// Add minutes to clock
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	c.minutes = toMinutes(c.getHours(), c.getMinutes()+m)
+	return c
 }
 
+// Subtract minutes from clock
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	c.minutes = toMinutes(c.getHours(), c.getMinutes()-m)
+	return c
+}
+
+func toMinutes(h, m int) int {
+	mins := (h*hourMinutes + m) % dayMinutes
+	if mins < 0 {
+		mins = dayMinutes + (mins % dayMinutes)
+	}
+	return mins
+}
+
+func (c Clock) getHours() int {
+	return c.minutes / hourMinutes % dayMinutes
+}
+
+func (c Clock) getMinutes() int {
+	return c.minutes % hourMinutes
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	return fmt.Sprintf("%02d:%02d", c.getHours(), c.getMinutes())
 }
