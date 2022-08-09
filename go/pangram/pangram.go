54e888f05268b7c35192d09c204905bc4d48f374
Sebastian Górecki <sebastian.gorecki@olx.pl>
2022-08-09 17:42:09 +0200

    golang: adds Pangram solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,28 @@
 package pangram
 
+import (
+	"unicode"
+)
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	var histogram = map[rune]int{}
+	for r := 'a'; r <= 'z'; r++ {
+		histogram[r] = 0
+	}
+
+	for _, r := range input {
+		l := unicode.ToLower(r)
+		if !unicode.IsLetter(l) {
+			continue
+		}
+		histogram[l]++
+	}
+
+	for _, v := range histogram {
+		if v == 0 {
+			return false
+		}
+	}
+
+	return true
 }
